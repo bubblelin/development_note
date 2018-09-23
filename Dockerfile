9b2106695e86a4cd3c9483693284e2cabeda29a9
bubble <yl.anin@qq.com>
2018-09-23 13:00:10 +0800

    新增Dockerfile文件构建镜像

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,3 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 COPY . /usr/src/app
-
-RUN echo "Asia/Shanghai" > /etc/timezone
-
-RUN cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
-echo "Suceessfully" >> ./README.md
--- build.sh
+++ build.sh
@@ -4,17 +4,20 @@ set +e
 
 echo '>>> Get old container id'
 
+echo '${BUILD_NUMBER}'
+echo %BUILD_NUMBER%
+
 CID=$(docker ps | grep "preview_development_note" | awk '{print $1}')
 
 echo $CID
 
-docker build -t localhost:5000/development_note:$BUILD_NUMBER .
+docker build -t localhost:5000/development_note:%BUILD_NUMBER% .
 
-docker push localhost:5000/development_note:$BUILD_NUMBER
+docker push localhost:5000/development_note:%BUILD_NUMBER%
 
 docker rm -f $(docker ps -a | grep $CID) || true
 
-docker run --name $CID localhost:5000/development_note:$BUILD_NUMBER
+docker run --name $CID localhost:5000/development_note:%BUILD_NUMBER%
 
 echo '>>> Starting new container: $CID'
 
