155219b4d2ddc64866dd798b287fb05e4001d4ef
bubble <yl.anin@qq.com>
2018-09-23 11:33:40 +0800

    新增Dockerfile文件构建镜像

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set +e
+
+echo '>>> Get old container id'
+
+CID=$(docker ps | grep "preview_development_note" | awk '{print $1}')
+
+echo $CID
+
+docker build -t localhost:5000/development_note:$BUILD_NUMBER .
+
+docker push localhost:5000/development_note:$BUILD_NUMBER
+
+docker rm -f $(docker ps -a | grep $CID) || true
+
+docker run --name $CID localhost:5000/development_note:$BUILD_NUMBER
+
+echo '>>> Starting new container: $CID'
+
+exit
